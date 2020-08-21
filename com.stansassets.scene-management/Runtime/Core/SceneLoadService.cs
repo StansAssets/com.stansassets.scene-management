a05f8b3c938929e9d0f7bb7b52db0438987caef4
fopnazarenko <fop.nazarenko.ivan@gmail.com>
2020-08-21 15:15:59 +0300

    Load/Unload scenes from addresables was added

--- com.stansassets.scene-management/Runtime/Core/SceneLoadService.cs
+++ com.stansassets.scene-management/Runtime/Core/SceneLoadService.cs
@@ -1,11 +1,14 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+ using UnityEngine.AddressableAssets;
 
 namespace StansAssets.SceneManagement
 {
     public class SceneLoadService : ISceneLoadService
     {
+        private Dictionary<string, Scene?> loadedScenes = new Dictionary<string, Scene?>();
         public void Load(IScenePreloader preloader, string sceneName, Action<ISceneManager> onComplete)
         {
             preloader.FadeIn(() =>
@@ -20,6 +23,39 @@ namespace StansAssets.SceneManagement
             });
         }
 
+        public void LoadFromAddresables(string sceneName, Action<ISceneManager> onComplete)
+        {
+            if (!loadedScenes.ContainsKey(sceneName))
+            {
+                loadedScenes[sceneName]=null;
+                Addressables.LoadSceneAsync(sceneName, LoadSceneMode.Additive).Completed += (sceneRequest) =>{
+                    if (sceneRequest.Status == AsyncOperationStatus.Succeeded)
+                    {
+                        var scene = sceneRequest.Result.Scene;
+                        var sceneManager = FindMonoTypeOnSceneRoot<ISceneManager>(scene);
+                        var sceneDelegate = FindMonoTypeOnSceneRoot<ISceneDelegate>(scene);
+                        loadedScenes[sceneName] = scene;
+                        if (sceneDelegate != null)
+                        {
+                            sceneDelegate.OnSceneLoaded();
+                            sceneDelegate.ActivateScene(() =>
+                            {
+                                onComplete?.Invoke(sceneManager);
+                            });
+                        }
+                        else
+                        {
+                            onComplete?.Invoke(sceneManager);
+                        }
+                    }
+                    else
+                    {
+                        loadedScenes.Remove(sceneName);
+                    }
+                };
+            }
+        }
+        
         public void Load(string sceneName, Action<ISceneManager> onComplete)
         {
             AdditiveScenesLoader.LoadAdditively(sceneName, scene =>
@@ -58,6 +94,24 @@ namespace StansAssets.SceneManagement
             }
         }
 
+        public void UnloadFromAddresables(string sceneName, Action onComplete)
+        {
+            //loadedScenes[sceneName] may be null while scene is loading.
+            if (loadedScenes.ContainsKey(sceneName) && loadedScenes[sceneName]!=null)
+            {
+                var scene = loadedScenes[sceneName].Value;
+                loadedScenes.Remove(sceneName);
+                var sceneDelegate = FindMonoTypeOnSceneRoot<ISceneDelegate>(scene);
+                sceneDelegate?.OnSceneUnload();
+                AdditiveScenesLoader.Unload(scene, onComplete);
+            }
+            else
+            {
+                // TODO error and optional param with scene action stack
+                onComplete?.Invoke();
+            }
+        }
+        
         public void Unload(string sceneName, Action onComplete)
         {
             if (AdditiveScenesLoader.TryGetLoadedScene(sceneName, out var scene))
