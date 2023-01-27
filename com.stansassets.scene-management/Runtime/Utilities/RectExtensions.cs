13a17794b010c7690e215d71c986c2fe89adb54e
Anastasiia Sotnykova <111708295+anastasiia-sotnykova@users.noreply.github.com>
2023-01-27 16:35:20 +0200

    feat: change platform editor UI layout (#41)
    * feat: change platform editor UI layout
    
    * fix: scene configuration item ui layout overlaps fixed
    
    Co-authored-by: Alexey Yaremenko <alex.g.yaremenko@gmail.com>

---
+++ com.stansassets.scene-management/Runtime/Utilities/RectExtensions.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace StansAssets.SceneManagement.Utilities
+{
+    public static class RectExtensions
+    {
+        public static Rect WithWidth(this Rect @this, float width)
+        {
+            return new Rect(@this.x, @this.y, width, @this.height);
+        }
+        
+        public static Rect WithHeight(this Rect @this, float height)
+        {
+            return new Rect(@this.x, @this.y, @this.width, height);
+        }
+        
+        public static Rect WithSize(this Rect @this, Vector2 size)
+        {
+            return new Rect(@this.x, @this.y, size.x, size.y);
+        }
+
+        public static Rect RightOf(this Rect @this, Rect other)
+        {
+            return new Rect(other.x + other.width, @this.y, @this.width, @this.height);
+        }
+
+        public static Rect ShiftHorizontally(this Rect @this, float offset)
+        {
+            return new Rect(@this.x + offset, @this.y, @this.width, @this.height);
+        }
+        
+        public static Rect ShiftVertically(this Rect @this, float offset)
+        {
+            return new Rect(@this.x, @this.y + offset, @this.width, @this.height);
+        }
+    }
+}
