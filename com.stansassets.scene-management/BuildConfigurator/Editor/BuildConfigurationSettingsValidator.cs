9a74dd4be82b05205affa13e2220d18bc30f336c
Yurii Lunha <yurii.lunha@stansassets.com>
2023-01-31 10:24:25 +0000

    Cleaned up unused code;

--- com.stansassets.scene-management/BuildConfigurator/Editor/BuildConfigurationSettingsValidator.cs
+++ com.stansassets.scene-management/BuildConfigurator/Editor/BuildConfigurationSettingsValidator.cs
@@ -14,11 +14,6 @@ namespace StansAssets.SceneManagement.Build
         static void OnPlayModeStateChanged(PlayModeStateChange state) {
             switch (state) {
                 case PlayModeStateChange.EnteredPlayMode:
-                    // TODO: Do we really need to sync scenes on play mode state changed?
-                    // if (BuildConfigurationSettings.Instance.HasValidConfiguration) {
-                    //     BuildConfigurationSettings.Instance.Configuration
-                    //         .SetupEditorSettings(EditorUserBuildSettings.activeBuildTarget, false);
-                    // }
                     break;
             }
         }
