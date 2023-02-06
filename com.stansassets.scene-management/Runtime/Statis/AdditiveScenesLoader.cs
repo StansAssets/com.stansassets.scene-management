7b25e8cd41603784e1703260a691f823b5c55042
Yurii Lunha <yurii.lunha@stansassets.com>
2023-02-06 12:20:47 +0100

    Throw an error when trying to load a scene in "play" mode that in not in the plugin configuration

--- com.stansassets.scene-management/Runtime/Statis/AdditiveScenesLoader.cs
+++ com.stansassets.scene-management/Runtime/Statis/AdditiveScenesLoader.cs
@@ -459,14 +459,7 @@ namespace StansAssets.SceneManagement
 
         static bool ValidateScene(string sceneName)
         {
-            // TODO let's reconsider the validation
-            /*
-            if (!Application.isEditor && BuildConfigurationSettings.Instance.HasValidConfiguration)
-            {
-                return BuildConfigurationSettings.Instance.Configuration.HasScene(sceneName);
-            }*/
-
-            return true;
+            return BuildConfigurationSettings.Instance.Configuration.HasScene(sceneName);
         }
     }
 
