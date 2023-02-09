3a4ee3f82bd96a7a0993faa194ea0ccd4215764f
Yurii Lunha <yurii.lunha@stansassets.com>
2023-02-09 21:22:31 +0100

    Implemented static message panel, and refactored some code.

--- com.stansassets.scene-management/BuildConfigurator/Editor/BuildConfigurationSettingsValidator.cs
+++ com.stansassets.scene-management/BuildConfigurator/Editor/BuildConfigurationSettingsValidator.cs
@@ -102,5 +102,40 @@ namespace StansAssets.SceneManagement.Build
                     break;
             }
         }
+        
+        /// <summary>
+        /// Check current Editor Build Settings with the Scene Management build configuration to prevent out of sync scenes.
+        /// </summary>
+        /// <returns>True - if scenes are out of sync</returns>
+        internal static bool CompareScenesWithBuildSettings()
+        {
+            var needToSync = BuildConfigurationSettings.Instance.Configuration
+                .CheckIntersectScenesWhBuildSettings(EditorUserBuildSettings.activeBuildTarget);
+
+            return needToSync;
+        }
+        
+        static void EditorBuildSettingsOnSceneListChanged()
+        {
+            if (!CompareScenesWithBuildSettings())
+            {
+                return;
+            }
+
+            BuildConfigurationMenu.OpenBuildSettings();
+            Debug.LogError($"Current Editor Build Settings are our of sync with the Scene Management " +
+                           $"build configuration. Scenes can be synchronized through the " +
+                           $"'Scene Management -> Build Settings'.");
+        }
+
+        internal static bool HasMissingScenes()
+        {
+            if (!BuildConfigurationSettings.Instance.HasValidConfiguration) return false;
+
+            return BuildConfigurationSettings.Instance.Configuration
+                       .DefaultScenes.Any(s => s != null && string.IsNullOrEmpty(s.Guid)) 
+                   || BuildConfigurationSettings.Instance.Configuration
+                       .Platforms.Any(p => p.Scenes.Any(s => s != null && string.IsNullOrEmpty(s.Guid)));
+        }
     }
 }
