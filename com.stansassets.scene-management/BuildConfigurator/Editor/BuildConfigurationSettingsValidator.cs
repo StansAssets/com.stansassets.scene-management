e2646ea5ecaa14ee62f4ed47a355ffecf3749092
Yurii Lunha <yurii.lunha@stansassets.com>
2023-02-10 09:19:21 +0100

    Implemented preventing window and an toggle in settings to control it

--- com.stansassets.scene-management/BuildConfigurator/Editor/BuildConfigurationSettingsValidator.cs
+++ com.stansassets.scene-management/BuildConfigurator/Editor/BuildConfigurationSettingsValidator.cs
@@ -59,12 +59,12 @@ namespace StansAssets.SceneManagement.Build
                        .Platforms.Any(p => p.Scenes.Any(s => s != null && string.IsNullOrEmpty(s.Guid)));
         }
 
-        public static bool HasScenesDuplicates()
+        internal static bool HasScenesDuplicates()
         {
             if (!BuildConfigurationSettings.Instance.HasValidConfiguration) return false;
             
             var hasDuplicates = BuildConfigurationSettings.Instance.Configuration
-                .GetDuplicateScenes(EditorUserBuildSettings.activeBuildTarget).Any();
+                .GetDuplicateScenes().Any();
 
             return hasDuplicates;
         }
@@ -101,51 +101,6 @@ namespace StansAssets.SceneManagement.Build
                     BuildConfigurationSettingsConfig.ShowOutOfSyncPreventingDialog = false;
                     break;
             }
-        }
-        
-        /// <summary>
-        /// Check current Editor Build Settings with the Scene Management build configuration to prevent out of sync scenes.
-        /// </summary>
-        /// <returns>True - if scenes are out of sync</returns>
-        internal static bool CompareScenesWithBuildSettings()
-        {
-            var needToSync = BuildConfigurationSettings.Instance.Configuration
-                .CheckIntersectScenesWhBuildSettings(EditorUserBuildSettings.activeBuildTarget);
-
-            return needToSync;
-        }
-        
-        static void EditorBuildSettingsOnSceneListChanged()
-        {
-            if (!CompareScenesWithBuildSettings())
-            {
-                return;
-            }
-
-            BuildConfigurationMenu.OpenBuildSettings();
-            Debug.LogWarning($"Current Editor Build Settings are our of sync with the Scene Management " +
-                           $"build configuration. Scenes can be synchronized through the " +
-                           $"'Scene Management -> Build Settings'.");
-        }
-
-        internal static bool HasMissingScenes()
-        {
-            if (!BuildConfigurationSettings.Instance.HasValidConfiguration) return false;
-
-            return BuildConfigurationSettings.Instance.Configuration
-                       .DefaultScenes.Any(s => s != null && string.IsNullOrEmpty(s.Guid)) 
-                   || BuildConfigurationSettings.Instance.Configuration
-                       .Platforms.Any(p => p.Scenes.Any(s => s != null && string.IsNullOrEmpty(s.Guid)));
-        }
-
-        internal static bool HasScenesDuplicates()
-        {
-            if (!BuildConfigurationSettings.Instance.HasValidConfiguration) return false;
-            
-            var hasDuplicates = BuildConfigurationSettings.Instance.Configuration
-                .GetDuplicateScenes().Any();
-
-            return hasDuplicates;
         }
     }
 }
