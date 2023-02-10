12c389bc114d949f5222c21ee93c0b0e37b4b1cf
Stanislav Osipov <stan@stansassets.com>
2023-02-10 13:38:23 +0200

    chore: "All good" message updated.

--- com.stansassets.scene-management/BuildConfigurator/Editor/BuildConfigurationWindow.cs
+++ com.stansassets.scene-management/BuildConfigurator/Editor/BuildConfigurationWindow.cs
@@ -450,7 +450,7 @@ namespace StansAssets.SceneManagement.Build
                     return;
                 }
                 
-                DrawMessage("All good", MessageType.Info);
+                DrawMessage("No issues found with the configuration.", MessageType.Info);
             }
         }
 
