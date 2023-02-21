ebf53f7f079b853cd5b03a236342f0d7acee6149
Yurii Lunha <yurii.lunha@stansassets.com>
2023-02-21 09:40:29 +0100

    Format code;

--- com.stansassets.scene-management/BuildConfigurator/Editor/BuildConfigurationMenu.cs
+++ com.stansassets.scene-management/BuildConfigurator/Editor/BuildConfigurationMenu.cs
@@ -4,17 +4,17 @@ namespace StansAssets.SceneManagement.Build
 {
     public static class BuildConfigurationMenu
     {
-        private static BuildConfigurationWindow m_Window;
+        static BuildConfigurationWindow s_Window;
 
         [MenuItem(SceneManagementPackage.RootMenu + "Build Settings", false, 1)]
         public static void OpenBuildSettings()
         {
-            if (m_Window == null)
+            if (s_Window == null)
             {
-                m_Window = BuildConfigurationWindow.ShowTowardsInspector("Build Conf");
+                s_Window = BuildConfigurationWindow.ShowTowardsInspector("Build Conf");
             }
 
-            m_Window.Show();
+            s_Window.Show();
         }
     }
 }
